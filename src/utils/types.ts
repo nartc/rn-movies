666e0cf99e3af10732caf02684480eb34ab7f21b
Chau <ctch5@mail.umsl.edu>
2019-06-27 18:15:34 -0500

    clean up

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,12 +1,7 @@
 import { ComponentType } from 'react';
-import { ActionType } from 'typesafe-actions';
 import { NavigationScreenComponent, NavigationStackScreenOptions, NavigationScreenProps } from 'react-navigation';
 
-export type ActionTypeWithout<T, K extends keyof T> = ActionType<Omit<T, K>>;
 export type StackScreenProps<TParams = {}> = NavigationScreenProps<TParams, NavigationStackScreenOptions>;
-export type StackScreenComponent<TProps = {}, TParams = {}> = NavigationScreenComponent<TParams,
-  NavigationStackScreenOptions,
-  TProps>;
-
+export type StackScreenComponent<TProps = {}, TParams = {}> = NavigationScreenComponent<TParams, NavigationStackScreenOptions, TProps>;
 export type Merge<M, N> = Omit<M, Extract<keyof M, keyof N>> & N;
 export type AnimatedComponent<T, N = {}> = ComponentType<Merge<T, N>>;
